d21f9ca83b5b68e069f50722a291caea639eb12a
avinash singh <avinash8.as@gmail.com>
2022-10-24 18:00:58 -0400

    added click alert

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const h2 = document.createElement("h2")
-h2.textContent = "Background"
-document.querySelector("body").appendChild(h2)
+const input = document.getElementById('button');
 
-const div = document.createElement("div")
-div.textContent = "I went to St. John's Univeristy for pharmacy school. I graduated in 2015, and have worked as a pharmacist since then. Working as a pharmacist has been very rewarding. I learned so much as a pharmacist. I recently started to learn to code and fell in love with it. I hope to start a new career as a software developer. "
-document.querySelector("body").appendChild(div)
+function clickAlert() {
+  alert('Thank you for reading! I hope you enojoyed reading my personal website');
+}
+
+input.addEventListener('click', clickAlert);
+
+
+
+
 
 
 
