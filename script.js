0ad597930a3358ba6c3af7affa1f40403f328ada
avinash singh <avinash8.as@gmail.com>
2022-10-24 16:50:14 -0400

    add background section and center first header

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const h2 = document.createElement("h2")
-h2.textContent = "This content added by Javascript"
+h2.textContent = "Background"
 document.querySelector("body").appendChild(h2)
+
+const div = document.createElement("div")
+div.textContent = "I went to St. John's Univeristy for pharmacy school. I graduated in 2015, and have worked as a pharmacist since then. Working as a pharmacist has been very rewarding. I learned so much as a pharmacist. I recently started to learn to code and fell in love with it. I hope to start a new career as a software developer. "
+document.querySelector("body").appendChild(div)
+
+
+
+
